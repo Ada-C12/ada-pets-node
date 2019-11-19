ef2a67cf86f2f9f09a87c721ce47a1a1d8d7c8f2
Sara Shah Baig <sarashahbaig@gmail.com>
2019-11-18 16:30:49 -0800

    completed

--- src/adaPets.js
+++ src/adaPets.js
@@ -1,6 +1,6 @@
 // Use Node-style imports for dependencies.
-const axios = require('axios');
-const result = require('./result.js');
+const axios = require("axios");
+const result = require("./result.js");
 
 const setResult = result.setResult;
 const setError = result.setError;
@@ -10,29 +10,64 @@ const BASE_URL = "https://petdibs.herokuapp.com/pets/";
 // Option functions.
 const listPets = () => {
   // Fill out as part of Wave 1.
-}
+  axios
+    .get(BASE_URL)
+    .then(response => {
+      // console.log(response.data);
+      return setResult(response.data);
+    })
+    .catch(error => {
+      return setError("Invalid Input");
+    });
+};
 
-const showDetails = (selectedPet) => {
+const showDetails = selectedPet => {
   if (!selectedPet) {
     setError("You tried to show details for a pet without selecting it!");
     return;
   }
-
   // Fill out as part of Wave 2.
-}
+  axios
+    .get(BASE_URL + selectedPet)
+    .then(response => {
+      console.log("Successful");
+      setResult(response.data);
+    })
+    .catch(error => {
+      return setError("Failed, could not show details, select somthing else");
+    });
+};
 
-const removePet = (selectedPet) => {
+const removePet = selectedPet => {
   if (!selectedPet) {
     setError("You tried to remove a pet without selecting it!");
     return;
   }
 
   // Fill out as part of Wave 3.
-}
+  axios
+    .delete(BASE_URL + selectedPet)
+    .then(response => {
+      console.log("Successfully deleted");
+      return setResult(response.data);
+    })
+    .catch(error => {
+      return setError("Failed, to remove");
+    });
+};
 
-const addPet = (petInfo) => {
+const addPet = petInfo => {
   // Fill out as part of Wave 4.
-}
+  axios
+    .post(BASE_URL, petInfo)
+    .then(response => {
+      console.log("Successfully added");
+      return setResult(petInfo);
+    })
+    .catch(error => {
+      return setError("failed, to add a pet");
+    });
+};
 
 // Use Node-style exports to export functions for tests and main.
 module.exports = {
@@ -40,4 +75,4 @@ module.exports = {
   showDetails: showDetails,
   removePet: removePet,
   addPet: addPet
-}
+};
--- test/wave3.test.js
+++ test/wave3.test.js
@@ -1,18 +1,18 @@
-const axios = require('axios');                    // Import axios.
-const MockAdapter = require('axios-mock-adapter'); // This is kind of like VCR.
-const result = require('../src/result.js');        // Import result handling.
+const axios = require("axios"); // Import axios.
+const MockAdapter = require("axios-mock-adapter"); // This is kind of like VCR.
+const result = require("../src/result.js"); // Import result handling.
 const setHandlers = result.setHandlers;
 
 //b Import our function(s) for testing.
-const adaPets = require('../src/adaPets.js');
+const adaPets = require("../src/adaPets.js");
 const removePet = adaPets.removePet;
 
 // This sets the mock adapter on the default instance
 const mock = new MockAdapter(axios);
 
-const fail = (error) => {
+const fail = error => {
   throw new Error(`Test failed! ${error}`);
-}
+};
 
 describe("Wave 3", () => {
   // Set up axios test responses.
@@ -24,8 +24,10 @@ describe("Wave 3", () => {
 
       // Assertions come first because they need to be ready before the function call.
       setHandlers(
-        done, // No assertions.  We just care that it finished.
-        fail);
+        () => done(), // No assertions.  We just care that it finished.
+        // No assertions.  We just care that it finished.
+        fail
+      );
 
       // Act.
       removePet(3);
@@ -33,21 +35,23 @@ describe("Wave 3", () => {
 
     it("sets an error string when there is no selected pet", done => {
       setHandlers(
-        fail,                   // Fail if we don't setError.
+        fail, // Fail if we don't setError.
         error => {
-          setTimeout(() => {    // We need this to consistently display assertion errors.
+          setTimeout(() => {
+            // We need this to consistently display assertion errors.
             // Assert.
             expect(error.constructor).toBe(String);
             expect(error).toMatch("remove");
             expect(error).toMatch("select");
 
             done();
-          })}
+          });
+        }
       );
 
       // Act.
       removePet();
-    })
+    });
 
     it("sets an error string when the response isn't successful", done => {
       // Arrange.
@@ -55,15 +59,17 @@ describe("Wave 3", () => {
       mock.onDelete("https://petdibs.herokuapp.com/pets/1000000").reply(404);
 
       setHandlers(
-        fail,                   // Fail if we don't setError.
+        fail, // Fail if we don't setError.
         error => {
-          setTimeout(() => {    // We need this to consistently display assertion errors.
+          setTimeout(() => {
+            // We need this to consistently display assertion errors.
             // Assert.
             expect(error).toMatch(/failed/i);
             expect(error).toMatch(/remove/i);
 
             done();
-          })}
+          });
+        }
       );
 
       // Act.
